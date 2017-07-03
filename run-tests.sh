28bfaf1f50c95d31d50041f4dea04566be79227d
Mathias Ewald <mewald@evoila.de>
2017-07-03 15:41:49 +0200

    Feature: Run tests as bash script

---
+++ run-tests.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+KEY_NAME=$1
+ENVIRONMENT=$2
+
+for TEST in `find tests -type f`; do
+  NAME=$(echo $TEST | gsed 's/tests\///g' | gsed 's/\//_/g' | gsed 's/.yaml$//g')
+  echo "TEST[$NAME]"
+  set -x
+  openstack stack create\
+    -t $TEST\
+    -e resources.yaml\
+    -e env/${ENVIRONMENT}.yaml\
+    --parameter key=$KEY_NAME\
+    --wait\
+    test-$NAME 2>&1
+  set +x
+done
